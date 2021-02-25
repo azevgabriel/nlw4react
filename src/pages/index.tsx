b4b5a35fd47ccaeb6ce0d6cf5d09fc4a47f4c731
Gabriel Azevedo <azevgabriel@gmail.com>
2021-02-25 10:01:20 -0300

    Aula 04

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { Profile } from '../components/Profile';
 import { ChallengeBox } from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 
 export default function Home() {
@@ -16,16 +17,18 @@ export default function Home() {
         <title>Inicio | Stretch</title>
       </Head>
       <ExperienceBar />
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   )
 }
