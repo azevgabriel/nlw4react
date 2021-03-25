a1a8f3c94939d1f7f3566ee87ecbea54bef42701
Gabriel Azevedo <azevgabriel@gmail.com>
2021-03-25 15:20:26 -0700

    PostgresSQL

---
+++ first_project_node/src/database/migrations/1616689518708-CreateAppointments.ts
@@ -0,0 +1,43 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateAppointments1616689518708 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'appointments',
+                columns:[
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: 'provider',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'data',
+                        type: 'timestamp with time zone',
+                        isNullable: false,
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointments')
+    }
+
+}
+
+/*
+    Linha do tempo:
+    Primeira semana: Agendamentos
+    Segunda seman: Usuário
+    (New dev) Terceira semana: Agendamentos
+    Quarta semana: Compras, sem atualizar os Agendamentos
+*/
