21085276f9588cc57cfac22ea1adb028c73aae06
Gabriel Azevedo <azevgabriel@gmail.com>
2021-03-26 06:27:45 -0700

    index, async and await functions Update

--- first_project_node/src/database/migrations/1616689518708-CreateAppointments.ts
+++ first_project_node/src/database/migrations/1616689518708-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1616689518708 implements MigrationInterfa
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
@@ -19,7 +20,7 @@ export default class CreateAppointments1616689518708 implements MigrationInterfa
                         isNullable: false,
                     },
                     {
-                        name: 'data',
+                        name: 'date',
                         type: 'timestamp with time zone',
                         isNullable: false,
                     }
