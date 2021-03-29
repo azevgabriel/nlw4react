d44544cae5fdf156718e7275773c526d5fa148a3
Gabriel Azevedo <azevgabriel@gmail.com>
2021-03-29 09:43:23 -0700

    New Migrations, Service and Model to User

--- first_project_node/src/database/migrations/1617026742400-CreateUsers.ts
+++ first_project_node/src/database/migrations/1617026742400-CreateUsers.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export default class CreateAppointments1616689518708 implements MigrationInterface {
+export default class CreateUsers1617026742400 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns:[
                     {
                         name: 'id',
@@ -15,12 +15,17 @@ export default class CreateAppointments1616689518708 implements MigrationInterfa
                         default: 'uuid_generate_v4()',
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
                     },
                     {
                         name:'created_at',
@@ -38,15 +43,7 @@ export default class CreateAppointments1616689518708 implements MigrationInterfa
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments')
+        await queryRunner.dropTable('users')
     }
 
 }
-
-/*
-    Linha do tempo:
-    Primeira semana: Agendamentos
-    Segunda seman: Usuário
-    (New dev) Terceira semana: Agendamentos
-    Quarta semana: Compras, sem atualizar os Agendamentos
-*/
--- first_project_node/src/models/Appointment.ts
+++ first_project_node/src/models/Appointment.ts
@@ -1,4 +1,11 @@
-import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
+import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn} from 'typeorm'
+import User from './Users';
+
+/*
+    One to One
+    One to Many (*) Invert
+    Many to Many
+*/
 
 @Entity('appointments')
 class Appointment {
@@ -7,11 +14,20 @@ class Appointment {
     id: string;
 
     @Column()
-    provider: string;
+    provider_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({name: 'provider_id'})
+    provider: User;
 
     @Column('timestamp with time zone')
     date: Date;
 
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
 }
 
 export default Appointment;
